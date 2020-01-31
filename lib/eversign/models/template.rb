c8d52a51367a311cdfbcb553edd604c207a83a63
Eduard Horiach <gorachedik96@gmail.com>
2020-01-31 06:57:41 +0200

    update tempalte.rb

--- lib/eversign/models/template.rb
+++ lib/eversign/models/template.rb
@@ -1,6 +1,11 @@
 module Eversign
   module Models
     class Template < Document
+      def initialize(template_id=nil, sandbox=nil, embedded_signing_enabled=nil)
+          self.template_id = template_id
+			    self.sandbox = sandbox
+			    self.embedded_signing_enabled = embedded_signing_enabled
+		  end 	
     end
   end
 end
